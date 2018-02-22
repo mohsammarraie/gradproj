7b3d84a46d128b3886c4d2086f2feef332afe94b
MOH <MOH@192.168.1.2>
2018-02-22 04:02:44 +0200

    fixed layout and added legend driver map.

--- src/java/beans/AddEditRouteStopBean.java
+++ src/java/beans/AddEditRouteStopBean.java
@@ -132,10 +132,9 @@ public class AddEditRouteStopBean implements Serializable {
             sessionBean.navigateManageRouteStops();
         } catch (Exception ex) {
 
-            error_message_header = "Error!";
-            error_message_content = ex.getMessage();
-
-            RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_ERROR, error_message_header, error_message_content));
+             //show error popup
+                RequestContext context = RequestContext.getCurrentInstance();
+                context.execute("PF('popup_add_edit_route_stop').show();");
             Logger.getLogger(AddEditRouteStopBean.class.getName()).log(Level.SEVERE, null, ex);
         }
 
